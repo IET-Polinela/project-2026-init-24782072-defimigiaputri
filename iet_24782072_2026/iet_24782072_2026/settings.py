8356a38421247e1d5afd3351e508f90d7d4c24ec
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-04-01 14:47:59 +0700

    (Lab3)Menghubungkan project Django dengan DBMS PostgreSQL pada settings.py.

--- iet_24782072_2026/iet_24782072_2026/settings.py
+++ iet_24782072_2026/iet_24782072_2026/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'iet_24782072_2026.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartcity_project',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

6b0c5a954c818e910bede5a8db361b48660991ac
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-04-01 16:41:50 +0700

    (Lab3)Melengkapi untuk fitur CRUD di views.py

--- iet_24782072_2026/main_app/views.py
+++ iet_24782072_2026/main_app/views.py
@@ -2,13 +2,43 @@ from django.shortcuts import render, redirect
 from .models import Report
 from .forms import ReportForm
 
+
+# CREATE
 def add_report(request):
     if request.method == "POST":
         form = ReportForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('report_list')
     else:
         form = ReportForm()
 
     return render(request, 'main_app/add_report.html', {'form': form})
+
+
+# READ
+def report_list(request):
+    reports = Report.objects.all()
+    return render(request, 'main_app/report_list.html', {'reports': reports})
+
+
+# UPDATE
+def update_report(request, id):
+    report = Report.objects.get(id=id)
+
+    if request.method == "POST":
+        form = ReportForm(request.POST, instance=report)
+        if form.is_valid():
+            form.save()
+            return redirect('report_list')
+    else:
+        form = ReportForm(instance=report)
+
+    return render(request, 'main_app/update_report.html', {'form': form})
+
+
+# DELETE
+def delete_report(request, id):
+    report = Report.objects.get(id=id)
+    report.delete()
+    return redirect('report_list')

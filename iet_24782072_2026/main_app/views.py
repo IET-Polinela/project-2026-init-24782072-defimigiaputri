6b2c70a5687f914b08332ad9b636e1c351cbcb1f
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-04-08 15:47:18 +0700

    (Lab4)Update CRUD dan Implementasi Workflow Dasar.

--- iet_24782072_2026/main_app/views.py
+++ iet_24782072_2026/main_app/views.py
@@ -1,10 +1,12 @@
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import View
+from django.urls import reverse_lazy
 from .models import Report
+from .forms import ReportForm
 
 
-# HOME
-from django.shortcuts import render
+# HOME (tetap FBV, tidak perlu diubah)
 def home(request):
     return render(request, 'main_app/home.html')
 
@@ -18,23 +20,22 @@ class ReportDetailView(DetailView):
 # CREATE
 class ReportCreateView(CreateView):
     model = Report
-    fields = ['title', 'category', 'description', 'location']
+    form_class = ReportForm
     template_name = 'main_app/add_report.html'
     success_url = reverse_lazy('report_list')
 
 
-# READ (List)
+# READ (LIST)
 class ReportListView(ListView):
     model = Report
     template_name = 'main_app/report_list.html'
     context_object_name = 'reports'
-    ordering = ['-created_at']
 
 
 # UPDATE
 class ReportUpdateView(UpdateView):
     model = Report
-    fields = ['title', 'category', 'description', 'location']
+    form_class = ReportForm
     template_name = 'main_app/update_report.html'
     success_url = reverse_lazy('report_list')
 
@@ -44,3 +45,13 @@ class ReportDeleteView(DeleteView):
     model = Report
     template_name = 'main_app/delete_report.html'
     success_url = reverse_lazy('report_list')
+
+
+# WORKFLOW STATUS (WAJIB)
+class ReportUpdateStatusView(View):
+    def post(self, request, pk):
+        report = get_object_or_404(Report, pk=pk)
+        new_status = request.POST.get('status')
+        report.status = new_status
+        report.save()
+        return redirect('report_list')

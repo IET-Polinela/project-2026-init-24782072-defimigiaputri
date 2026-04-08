039f27abed4bbf89f99a9fe9f0d8a9fc59b57c1a
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-04-08 15:01:25 +0700

    (Lab4)Implementasi Class-Based View pada CRUD sekaligus untuk melakukan migrasi setelah melakukan perubahan pada model.

--- iet_24782072_2026/main_app/views.py
+++ iet_24782072_2026/main_app/views.py
@@ -1,53 +1,46 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Report
-from .forms import ReportForm
 
 
 # HOME
+from django.shortcuts import render
 def home(request):
     return render(request, 'main_app/home.html')
 
 
+# DETAIL
+class ReportDetailView(DetailView):
+    model = Report
+    template_name = 'main_app/report_detail.html'
+
+
 # CREATE
-def add_report(request):
-    if request.method == "POST":
-        form = ReportForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('report_list')
-    else:
-        form = ReportForm()
-
-    return render(request, 'main_app/add_report.html', {'form': form})
+class ReportCreateView(CreateView):
+    model = Report
+    fields = ['title', 'category', 'description', 'location']
+    template_name = 'main_app/add_report.html'
+    success_url = reverse_lazy('report_list')
 
 
-# READ
-def report_list(request):
-    reports = Report.objects.all().order_by('-created_at')
-    return render(request, 'main_app/report_list.html', {'reports': reports})
+# READ (List)
+class ReportListView(ListView):
+    model = Report
+    template_name = 'main_app/report_list.html'
+    context_object_name = 'reports'
+    ordering = ['-created_at']
 
 
 # UPDATE
-def update_report(request, id):
-    report = get_object_or_404(Report, id=id)
-
-    if request.method == "POST":
-        form = ReportForm(request.POST, instance=report)
-        if form.is_valid():
-            form.save()
-            return redirect('report_list')
-    else:
-        form = ReportForm(instance=report)
-
-    return render(request, 'main_app/update_report.html', {'form': form})
+class ReportUpdateView(UpdateView):
+    model = Report
+    fields = ['title', 'category', 'description', 'location']
+    template_name = 'main_app/update_report.html'
+    success_url = reverse_lazy('report_list')
 
 
 # DELETE
-def delete_report(request, id):
-    report = Report.objects.get(id=id)
-
-    if request.method == 'POST':
-        report.delete()
-        return redirect('report_list')
-
-    return render(request, 'main_app/delete_report.html', {'report': report})
+class ReportDeleteView(DeleteView):
+    model = Report
+    template_name = 'main_app/delete_report.html'
+    success_url = reverse_lazy('report_list')

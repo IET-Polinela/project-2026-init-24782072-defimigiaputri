27a981d7e12d07c330392a806545f4196f0a1f34
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-04-22 14:59:31 +0700

    (Lab6) 1. Inisialisasi Custom User Model

--- iet_24782072_2026/iet_24782072_2026/settings.py
+++ iet_24782072_2026/iet_24782072_2026/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'main_app',
     'about',
     'contacts',
+    'usermanagement_24782072',
 
 ]
 
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'usermanagement_24782072.User'

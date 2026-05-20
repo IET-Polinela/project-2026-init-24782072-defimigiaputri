7c4a6bc4872df5c19448c6fc5e5231beff8d914b
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-05-20 15:24:02 +0700

    (Lab10) 4. Integrasi ViewSet dan Anonimitas Pelapor

--- iet_24782072_2026/main_app/api_views.py
+++ iet_24782072_2026/main_app/api_views.py
@@ -1,14 +1,85 @@
-from rest_framework import viewsets, permissions
+from rest_framework import (
+    viewsets,
+    permissions
+)
+
+from django.db.models import Q
 
 from .models import Report
 
 from .serializers import ReportSerializer
 
+from .permissions import (
+    IsOwnerAndDraftOrReadOnly
+)
 
-class ReportViewSet(viewsets.ModelViewSet):
 
-    permission_classes = [permissions.AllowAny]
-
-    queryset = Report.objects.all()
+class ReportViewSet(
+    viewsets.ModelViewSet
+):
 
     serializer_class = ReportSerializer
+
+
+    def get_queryset(self):
+
+        user = self.request.user
+
+
+        if user.is_superuser:
+
+            return Report.objects.exclude(
+                status='DRAFT'
+            )
+
+
+        return Report.objects.filter(
+
+            Q(status__in=[
+                'REPORTED',
+                'VERIFIED',
+                'IN_PROGRESS',
+                'RESOLVED'
+            ])
+
+            |
+
+            Q(
+                reporter=user,
+                status='DRAFT'
+            )
+
+        )
+
+
+    def get_permissions(self):
+
+        if self.action in [
+            'update',
+            'partial_update',
+            'destroy'
+        ]:
+
+            return [
+
+                permissions.IsAuthenticated(),
+
+                IsOwnerAndDraftOrReadOnly()
+
+            ]
+
+        return [
+
+            permissions.IsAuthenticated()
+
+        ]
+
+
+    def perform_create(
+        self,
+        serializer
+    ):
+
+        serializer.save(
+            reporter=self.request.user
+        )

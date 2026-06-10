8c261cf395c4b1657b31de6096782765e9f22a85
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-06-10 14:06:34 +0700

    (Lab13) Tahap 1 - 2. Penyesuaian Referensi Konfigurasi

--- server_smartcity/smartcity_app/wsgi.py
+++ server_smartcity/smartcity_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for temp project.
+WSGI config for smartcity_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartcity_app.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'smartcity_app.settings'
+)
 
 application = get_wsgi_application()

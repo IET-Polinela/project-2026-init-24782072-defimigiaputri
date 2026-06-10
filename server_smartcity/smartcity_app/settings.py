0e438d5967e90428c171eedc72cc55793f77a2a2
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-06-10 13:54:11 +0700

    (Lab13) Tahap 1 - 1. Penyesuaian Struktur Proyek

--- server_smartcity/smartcity_app/settings.py
+++ server_smartcity/smartcity_app/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'iet_24782072_2026.urls'
+ROOT_URLCONF = 'smartcity_app.urls'
 
 TEMPLATES = [
     {
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'iet_24782072_2026.wsgi.application'
+WSGI_APPLICATION = 'smartcity_app.wsgi.application'
 
 
 # Database

c4cb92eba8d470df93a58ca9917097bd477d7f14
init-24782072-defimigiaputri <defimigia@gmail.com>
2026-06-17 14:03:43 +0700

    (Lab14) 3. Konfigurasi Default Schema Class DRF

--- server_smartcity/smartcity_app/settings.py
+++ server_smartcity/smartcity_app/settings.py
@@ -148,6 +148,9 @@ LOGIN_URL = 'login'
 
 REST_FRAMEWORK = {
 
+    'DEFAULT_SCHEMA_CLASS':
+    'drf_spectacular.openapi.AutoSchema',
+
     'DEFAULT_RENDERER_CLASSES': [
 
         'rest_framework.renderers.JSONRenderer',
